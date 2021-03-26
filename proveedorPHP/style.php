a37f4ca35c9cac6c3b4df2117cc2fa8ae4c39d1c
Bryan <looez.2015@gmail.com>
2021-03-26 16:07:43 -0600

    Creamos el proveedorPHP
    agregamos el archivo style.php, el cual tiene su método constructor, los Setter y Getter, y otras funciones

---
+++ proveedorPHP/style.php
@@ -0,0 +1,48 @@
+<?php 
+
+class Style {
+  private $className;
+  private $style;
+
+  public function __construct($class, $style)
+  {
+    $this->className = $class;
+    $this->style = $style;
+  }
+
+  // SETTERS
+  public function establecerClase($clase)
+  {
+    $this->className = $clase;
+  }
+  public function establecerEstilo($estilo)
+  {
+    $this->style = $estilo;
+  }
+
+  // GETTERS
+  public function obtenerClase()
+  {
+    return $this->className;
+  }
+  public function obtenerEstilo()
+  {
+    return $this->style;
+  }
+
+  // FUNCIONES
+  public function agregarStyle()
+  {
+    $nombre = $this->obtenerClase();
+    $estilo = $this->obtenerEstilo();
+
+    return $nombre . '{' . $estilo . '}';
+  }
+
+  public function agregarQuery($query)
+  {
+    return $query;
+  }
+}
+
+?>
