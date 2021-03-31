256d59fbe7015511b5a263b07dafe7875a39f8bd
Bryan <looez.2015@gmail.com>
2021-03-30 19:24:17 -0600

    Hacemos una unión entre piezas
    lo que hacemos es unir los archivos header con el flip-card

--- proveedorHTML/header.php
+++ proveedorHTML/header.php
@@ -1,7 +1,8 @@
 <?php 
 
 require ('proveedorPHP/tag.php');
-$contenido = "<h1>Codigo de Oro PHP</h1>" ;
+require ('proveedorHTML/flip-card.php');
+$contenido = "<h1>Codigo de Oro PHP</h1>".$flipCardT->construirPieza();
 $encabezado = new Tag("<header>","header",$contenido);
 
 echo ($encabezado->construirPieza());
